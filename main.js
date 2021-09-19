c23466012922424e7afbd7dab22a731b4752ace4
coderaditisingh <86613587+coderaditisingh@users.noreply.github.com>
2021-09-19 22:26:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function next_page()
+{
+   window.location="index.html";
+}
+
+function teleport_to_Myself()
+{
+    window.location="myself.html";
+}
+
+function teleport_to_CWM()
+{
+    window.location="cwm.html";
+}
+
+function teleport_to_PWM()
+{
+    window.location="pwm.html";
+}
+
+function teleport_to_my_drawings()
+{
+    window.location="my_drawings.html";
+}
+
+function teleport_to_take_my_selfie()
+{
+    window.location="take_my_selfie.html";
+}
+
+function teleport_to_Ending()
+{
+    window.location="Ending.html";
+}
